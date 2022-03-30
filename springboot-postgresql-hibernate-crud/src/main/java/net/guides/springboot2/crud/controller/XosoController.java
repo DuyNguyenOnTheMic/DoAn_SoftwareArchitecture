02c94027be60fc3deac64542da3374229153dda5
DuyNTOnTheMic <duy.197pm21905@vanlanguni.vn>
2022-03-30 12:12:47 +0700

    Xoá những thứ dư thừa

--- springboot-postgresql-hibernate-crud/src/main/java/net/guides/springboot2/crud/controller/XosoController.java
+++ springboot-postgresql-hibernate-crud/src/main/java/net/guides/springboot2/crud/controller/XosoController.java
@@ -22,42 +22,4 @@ public class XosoController {
 		return xosoRepository.findAll();
 	}
 
-	/*
-	@GetMapping("/druginfo/{id}")
-	public ResponseEntity<Druginfo> getDruginfoById(@PathVariable(value = "id") String id)
-			throws ResourceNotFoundException {
-		Druginfo druginfo = druginfoRepository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + id));
-		return ResponseEntity.ok().body(druginfo);
-	}
-
-	@PostMapping("/employees")
-	public Druginfo createEmployee(@Valid @RequestBody Druginfo druginfo) {
-		return druginfoRepository.save(druginfo);
-	}
-
-	@PutMapping("/employees/{id}")
-	public ResponseEntity<Druginfo> updateEmployee(@PathVariable(value = "id") Long employeeId,
-                                                   @Valid @RequestBody Druginfo druginfoDetails) throws ResourceNotFoundException {
-		Druginfo druginfo = druginfoRepository.findById(employeeId)
-				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
-
-		druginfo.setEmailId(druginfoDetails.getEmailId());
-		druginfo.setLastName(druginfoDetails.getLastName());
-		druginfo.setFirstName(druginfoDetails.getFirstName());
-		final Druginfo updatedDruginfo = druginfoRepository.save(druginfo);
-		return ResponseEntity.ok(updatedDruginfo);
-	}
-
-	@DeleteMapping("/employees/{id}")
-	public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
-			throws ResourceNotFoundException {
-		Druginfo druginfo = druginfoRepository.findById(employeeId)
-				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
-
-		druginfoRepository.delete(druginfo);
-		Map<String, Boolean> response = new HashMap<>();
-		response.put("deleted", Boolean.TRUE);
-		return response;
-	}*/
 }
